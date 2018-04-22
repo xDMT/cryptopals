cb8f34202a077d100110c87626d7e41b347823ff
Nick Martinez <nick@xdmtk.org>
2018-04-22 01:54:42 -0700

    Clenaed up

--- set1/8detectaes.c
+++ set1/8detectaes.c
@@ -27,26 +27,31 @@ int main(int argc, char * argv[])
     {
 
         z++;
+        // Remember space for the null terminator!!
         byte buffer1[17], buffer2[17];
         buffer1[16] = '\0';
         buffer2[16] = '\0';
 
+
+        // Compare blocks 
         for (i = 0; i < len-32; i += 16)
         {
             memcpy(buffer1, line+i, 16);
 
+            // Second block starts 1 block past comparison
+            // block, position 'i'
             for (f = 16; f+i < len; f += 16)
             {
 
                 memcpy(buffer2, line+i+f, 16);
                 
-           //     printf("Comparing %s to %s for line %d\n", buffer1, buffer2, z); 
-                ret = strcmp(buffer1,buffer2);
-                if (!ret)
+                // On 0 return value, blocks are equal
+                // indicating ECB mode
+                if (!strcmp(buffer1,buffer2))
                 {
-                    printf("\nLine %d ECB Mode Detected: %s\n\n", z, line);
+                    printf("\n\n\nLine %d ECB Mode Detected\n", z, line);
                     printf("Repeated byte block #%d: %s\n", (i/16)+1, buffer1);
-                    printf("Repeated byte block #%d: %s\n", (i/16)+2, buffer2);
+                    printf("Repeated byte block #%d: %s\n", ((f+i)/16)+1, buffer2);
                 }
             }
         }
