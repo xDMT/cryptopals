112c311ef38aff7ad6d3aa1a546b8fbf8161b653
Nick Martinez <nick@xdmtk.org>
2019-02-04 23:29:22 -0800

    Finish 15

--- set2/15pcks.c
+++ set2/15pcks.c
@@ -4,37 +4,67 @@
 
 #define true 1
 #define false 0
+#define bool int
+
+unsigned char * pkcs_validate(unsigned char* padded_str, bool* valid);
+void output_validity(unsigned char* padded_str, bool* valid);
+
 
 int main() {
-
-
-
-
-
-
-
+    
+    bool valid = false;
+    unsigned char *test1 = "This is valid padding\x000B\x000B\x000B\x000B\x000B" \
+                            "\x000B\x000B\x000B\x000B\x000B\x000B";
+    
+    output_validity(test1, &valid);
+    
+    
     return 0;
 }
 
 
-int pkcs_validate(unsigned char* padded_str) {
+unsigned char * pkcs_validate(unsigned char* padded_str, bool* valid) {
    
     int len = strlen(padded_str);
     int i, pad_pos;
     unsigned char pad_val = padded_str[len-1];
+    unsigned char * new_str = (unsigned char *) calloc(len,sizeof(unsigned char));
 
+
+    memcpy(new_str, padded_str, len);
     if (pad_val == 0x1) {
-        pad_val[len-1] = '\0';
-        return true;
+        padded_str[len-1] = '\0';
+        *valid = true;
+        return new_str;
     }
-    pad_pos = len - pad_val - 1;
+    pad_pos = len - pad_val;
     for (i=pad_pos; i < len; ++i) {
+
+        // Validate padding values 
         if (padded_str[i] != pad_val) {
-            return false;
+            *valid = false;
+            return NULL;
         }
+
+        // For passing padding, strip the string by nulls
+        new_str[i] = '\0';
     }
-
-
-
+    *valid = true;
+    return new_str;
 
 }
+
+
+void output_validity(unsigned char* padded_str, bool* valid) {
+    
+    padded_str = pkcs_validate(padded_str, valid);
+    if (valid) {
+        printf("Padding is valid for string: %s\n", padded_str);
+    }
+    else {
+        printf("Padding not valid\n");
+    }
+    free(padded_str);
+
+}
+
